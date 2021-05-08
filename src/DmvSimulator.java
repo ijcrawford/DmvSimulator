9d4c98044f08bbbcc09c7351feb404efa35bf25f
ijcrawford <ijcrawford@quinnipiac.edu>
2021-05-08 11:21:37 -0400

    added miracle

--- src/DmvSimulator.java
+++ src/DmvSimulator.java
@@ -1,12 +1,14 @@
 public class DmvSimulator {
 
     public static void main(String[] args) {
-        int spot = (int) Math.random()*200;
+        int spot = (int) (Math.random()*200);
         System.out.println("*sigh* Hi, welcome to the DMV...\n" +
                 "Huh? You wanna get in line? Sure you're number... uhh...\n" +
                 "Number " + spot + "! Yeah.");
         for (int i = spot+1; i<201;i++) System.out.println("Calling number " + i + "...");
         for (int i = 1; i<spot+1;i++) System.out.println("Calling number " + i + "...");
+        if ((int)(Math.random()*100) != 50)
 	    	System.out.println("Acquaintance, no papers? Get out of here you miserable worm!!!!");
+        else System.out.println("Aight, you good.");
   	}
 }
